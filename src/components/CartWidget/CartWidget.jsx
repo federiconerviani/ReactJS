10903a122d045b49047c9966c57908d143df8035
Federico Nerviani <federiconerviani@gmail.com>
2023-05-03 16:44:02 -0300

    Contador

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,8 +1,10 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import estilosCart from "./CartWidget.module.css";
+
 const CartWidget = () => {
   return (
-    <div>
-      <AiOutlineShoppingCart size={30} color="blue" />
+    <div className={estilosCart.containerCart}>
+      <AiOutlineShoppingCart size={40} color="blue" />
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,22 @@
 import estilosNavbar from "./Navbar.module.css";
 import CartWidget from "../CartWidget/CartWidget";
+import Counter from "../Counter/Counter";
 
 const Navbar = ({ saludo, edad }) => {
   return (
     <div className={estilosNavbar.containerNavbar}>
       <img
+        className={estilosNavbar.logo}
         src="https://res.cloudinary.com/dd4cervcm/image/upload/v1680634494/logo1_nkrsdm.png"
         alt="Logo página"
       />
-      <ul style={{ display: "flex", gap: 50, listStyle: "none" }}>
+      <ul className={estilosNavbar.lista}>
         <li>Medicinales</li>
-        <li>Psicodélicos</li>
+        <li>Terapéuticos</li>
         <li>Gourmet</li>
-        <li>{saludo}</li>
-        <li>{edad}</li>
       </ul>
       <CartWidget />
+      <Counter />
     </div>
   );
 };
