13fa61ddb8fd2e7882c98c61264de82106e7faa3
Federico Nerviani <federiconerviani@gmail.com>
2023-05-19 18:12:53 -0300

    eliminar productos individuales del carrito OK

--- src/components/Context/CartContext.jsx
+++ src/components/Context/CartContext.jsx
@@ -6,8 +6,28 @@ export const CartContext = createContext();
 const CartContextProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
+  const isInCart = (id) => {
+    let existe = cart.some((elemento) => elemento.id === id);
+    return existe;
+  };
+
   const agregarAlCarrito = (producto) => {
-    setCart([...cart, producto]);
+    let existe = isInCart(producto.id);
+    if (existe) {
+      let newCart = cart.map((elemento) => {
+        if (elemento.id === producto.id) {
+          return {
+            ...elemento,
+            quantity: elemento.quantity + producto.quantity,
+          };
+        } else {
+          return elemento;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, producto]);
+    }
   };
 
   const clearCart = () => {
@@ -15,7 +35,9 @@ const CartContextProvider = ({ children }) => {
   };
 
   const deleteProductById = (id) => {
-    console.log(id);
+    console.error(id);
+    const productosFiltrados = cart.filter((elemento) => elemento.id !== id);
+    setCart(productosFiltrados);
   };
 
   let data = { cart, agregarAlCarrito, clearCart, deleteProductById };
